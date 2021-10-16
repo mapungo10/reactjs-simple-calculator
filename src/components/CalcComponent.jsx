7c9a7b773338b980fdc6d966ea14b43dfc0a1e1c
Denice Minja <mapungo10@gmail.com>
2021-10-16 19:58:43 +0300

    Align calc UI center (vertical).

--- src/components/CalcComponent.jsx
+++ src/components/CalcComponent.jsx
@@ -7,9 +7,9 @@ function CalcComponent(props) {
     return (
         <>
             <div className="container">
-                <div className="row justify-content-center align-items-center">
-                    <div className="col-sm-4 mt-5">
-                        <div className="calc-con bg-info p-2" style={{}}>
+                <div className="row justify-content-center align-items-center d-flex" style={{ height: '100vh' }}>
+                    <div className="col-sm-4">
+                        <div className="calc-con bg-info p-2">
                             <ScreenComponent screenInput={props.screenInput} />
                             <ButtonsComponent handleBtnClick={props.handleBtnClick}/>
                         </div>
